cd3e7d582effea7a4f2ffeb25a575b83a5ef8e9e
Ed Solis <edsfocci@gmail.com>
2013-10-10 19:29:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlashMobber::Application.config.secret_token = '89ff6f32f1e9b2dbbffccadfb50dfb1f2da725fff16c07219a87c4fd86734eb280d47dc24a63fa6056893f20fbca9e8a2ce69a491c64f8eb381b488b4733d446'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FlashMobber::Application.config.session_store :cookie_store, key: '_Flash_Mobber_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FlashMobber::Application.config.session_store :active_record_store
